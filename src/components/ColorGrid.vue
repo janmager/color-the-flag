0e54a2b6a66cc21a8a813e65b1d5956a8c3dbfd1
Jan Mager <jasiu.mager@gmail.com>
2019-04-17 13:16:10 +0200

    First commit

---
+++ src/components/ColorGrid.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="grid">
+        <div 
+            class="grid-item"
+            v-for='(block, index) in blocks'
+            :key='index'
+            :style="'background:'+block.color"
+            @click='setBlockColor(index)'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'color-grid',
+    props: ['color'],
+    data: function(){
+        return{
+            blocks: [],
+            gridSize: 100
+        }
+    },
+    methods: {
+        initGrid(){
+            for(let i = 0;i<this.gridSize; i++){
+                this.blocks.push({
+                    color: '#FFF'
+                })
+            }
+        },
+        setBlockColor(index){
+            this.blocks[index].color = this.color;
+        }
+    },
+    created(){
+        this.initGrid();
+    }
+}
+</script>
+
+<style scoped>
+    .grid{
+        display: grid;
+        grid-template-columns: repeat(10, 1fr);
+        grid-template-rows: repeat(10, 1fr);
+        width: 320px;
+        height: 320px;
+        margin: 0 auto;
+    }
+    .grid-item{
+        border: 1px dashed #CCC;
+    }
+</style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='color-picker'>
+        <div 
+            class='color'
+            v-for='(color, index) in colors' 
+            :key='index'
+            :style="'background:'+color"
+            @click='setColor(color)'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'color-picker',
+    props: ['colors'],
+    methods: {
+        setColor(color){
+            this.$emit('setColor', color);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .color-picker{
+        display: grid;
+        width: 125px;
+        margin: 15px auto;
+        grid-template-columns: repeat(5, 25px);
+    }
+    .color{
+        border: 1px dashed #000;
+        height: 25px;
+    }
+</style>
